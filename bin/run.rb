18c86265db9437b4b871c7165ce0c6a5b7011d37
gjbonner <gjbonner@gmail.com>
2018-07-12 16:28:41 -0400

    done

--- bin/run.rb
+++ bin/run.rb
@@ -13,71 +13,85 @@ def list_methods
 * Get most popular artist - 'most popular artist'
 * Get artist with most albums - 'most albums artist'
 * Display all artists - 'all artists'
-* Display all songs - 'all songs'
-* Display all albums - 'all albums'
+* Display all songs - 'all artist songs'
+* Display all albums - 'all artist albums'
 ------------------------------------------------
   STRING
-input = gets.chomp
+end
 
-if input == 'add artist'
-  puts "Enter an Artist name: "
-  artist = Method.gets_user_input
-  # binding.pry
-  fan_count = Method.find_num_fans(artist)
-  # binding.pry
-  album_count = Method.find_num_albums(artist)
-  # binding.pry
-  if !Artist.exists?(name: artist)
-    artist_instance = Artist.create_artist(artist, album_count, fan_count)
-  else
-      puts "Artist is already in table."
-  end
-elsif input == 'make playlist'
-  puts "Enter a playlist name: "
-  name = gets.chomp
-  puts "Enter the artist name: "
-  artist = gets.chomp
-  puts "Enter a song name: "
-  song = gets.chomp
+while true
+  puts "Enter help if you wish to view menu, otherwise press enter"
+  input = gets.chomp
+  if input == "help"
+  list_methods
+end
+  puts "Enter a command"
+  input = gets.chomp
+  if input == 'add artist'
+    puts "Enter an Artist name: "
+    artist = Method.gets_user_input
+    # binding.pry
+    fan_count = Method.find_num_fans(artist)
+    # binding.pry
+    album_count = Method.find_num_albums(artist)
+    # binding.pry
+    if !Artist.exists?(name: artist)
+      artist_instance = Artist.create_artist(artist, album_count, fan_count)
+    else
+        puts "Artist is already in table."
+    end
+  elsif input == 'make playlist'
+    puts "Enter a playlist name: "
+    name = gets.chomp
+    puts "Enter the artist name: "
+    artist = gets.chomp
+    puts "Enter a song name: "
+    song = gets.chomp
 
-  artist_id = Playlist.get_artist_id(artist)
-  song_id = Playlist.get_song_id(song)
+    artist_id = Playlist.get_artist_id(artist)
+    song_id = Playlist.get_song_id(song)
 
-  if artist_id && song_id
-    Playlist.create_playlist(name, artist_id, song_id)
-  end
-elsif input == 'find artist'
-  puts "Enter the artist name: "
-  name = gets.chomp
-  name = Method.input(name)
-  artist = Artist.find_by(name: name)
-elsif input == 'find album'
-  puts "Enter the album name: "
-  name = gets.chomp
-  name = Method.input(name)
-  p album = Album.find_by(name: name)
-elsif input == 'find song'
-  puts "Enter the song name: "
-  name = gets.chomp
-  name = Method.input(name)
-  song = Song.find_by(name: name)
-elsif input == 'most popular'
-  Artist.most_popular
-elsif input == 'most albums'
-  Artist.most_albums
-elsif input == 'all artist songs'
-  puts Song.all_songs
-elsif input == 'all artists'
-  puts Artist.all_artists
-elsif input == 'all artist albums'
+    if artist_id && song_id
+      Playlist.create_playlist(name, artist_id, song_id)
+    end
+  elsif input == 'find artist'
+    puts "Enter the artist name: "
+    name = gets.chomp
+    name = Method.input(name)
+    artist = Artist.find_by(name: name)
+  elsif input == 'find album'
+    puts "Enter the album name: "
+    name = gets.chomp
+    name = Method.input(name)
+    p album = Album.find_by(name: name)
+  elsif input == 'find song'
+    puts "Enter the song name: "
+    name = gets.chomp
+    name = Method.input(name)
+    song = Song.find_by(name: name)
+  elsif input == 'most popular'
+    Artist.most_popular
+  elsif input == 'most albums'
+    Artist.most_albums
+  elsif input == 'all artist songs'
     puts "Enter an artist"
     input = gets.chomp
-   puts Album.all_albums(input)
-end
+    puts Song.all_songs(input)
+  elsif input == 'all artists'
+    puts Artist.all_artists
+  elsif input == 'all artist albums'
+      puts "Enter an artist"
+      input = gets.chomp
+     puts Album.all_albums(input)
+   elsif input == "exit"
+     break
+   else
+     puts "Invalid command."
+  end
 
-end
+  end
 
-list_methods
+
 #
 # puts "Enter an artist name"
 # artist = gets.chomp
